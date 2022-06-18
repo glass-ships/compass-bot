3cf70f20330575a878fb1b164cc85eedaab64bad
glass-ships <glass.ships@outlook.com>
2022-06-17 22:39:32 -0600

    add some libs to maybe fix DO voice functionality

--- src/main.py
+++ src/main.py
@@ -39,36 +39,6 @@ async def getprefix(bot, ctx):
         prefix = DEFAULT_PREFIX
     return commands.when_mentioned_or(prefix)(bot,ctx)
 
-# Custom help commands
-class CustomHelpCommand(commands.HelpCommand):
-    def __init__(self) -> None:
-        super().__init__()
-    
-    async def send_bot_help(self, mapping):
-        emb = discord.Embed(
-            title="Compass Bot Help",
-            description="Looking for a commands list?\nNeed help setting up Compass in your server?"
-        )
-        emb.add_field(name="See the documentation:", value="[Compass docs](https://glass-ships.gitlab.io/compass-bot)")
-        emb.set_thumbnail(url='https://gitlab.com/glass-ships/compass-bot/-/raw/main/docs/images/compass.png')
-                
-        await self.get_destination().send(embed=emb)
-    
-    async def send_cog_help(self, cog):
-        return await super().send_cog_help(cog)
-        output = []
-        for cog in mapping:
-            cogname = cog.qualified_name if cog else "None"
-            if cog:
-                cmds = cog.get_commands()
-                output.append(f"{cogname}: {[cmd.name for cmd in cmds]}")            
-        emb.add_field(name="Cogs", value=[cog.qualified_name for cog in mapping], inline=False)
-
-    async def send_group_help(self, group):
-        return await super().send_group_help(group)
-    
-    async def send_command_help(self, command):
-        return await super().send_command_help(command)
 
 # Setup music functionality
 async def config_music(guild):
@@ -107,7 +77,7 @@ async def config_music(guild):
 
 bot = commands.Bot(
     application_id = 932737557836468297, # main bot
-    #application_id = 535346715297841172, # test bot
+    application_id = 535346715297841172, # test bot
     help_command=CustomHelpCommand(),
     command_prefix = getprefix,
     description = "A general use and moderation bot in Python.",
